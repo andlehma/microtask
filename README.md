b9ce184dc1ba84fe32bf84bedab335ffcc120bf2
andlehma <andrew.lehman364@gmail.com>
2020-01-27 17:04:07 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# react boilerplate
+about as minimal as i could make it
+
+# installation
+* clone the repo
+* `npm install`
+* `npm run build`
---
+++ components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        App Placeholder
+      </div>
+    )
+  }
+}
+
+module.exports = App;
