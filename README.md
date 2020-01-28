ea9d52cf30279de57ccb3b8906b741c48b5b407a
Andrew <andrew.lehman364@gmail.com>
2020-01-28 01:44:59 -0600

    implement create/delete tasks

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # microtask
 frontend-only version of [minitask](https://github.com/andlehma/minitask)
+
+# todo
+ * implement editing task
+ * split App.js into multiple components
+ * sort task list by due date
--- components/App.js
+++ components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import NewTaskButton from './NewTaskButton';
+let nextId = 0;
 
 class Task {
-  constructor(id, title, course, assigned, due) {
+  constructor(id, title, course, due) {
     this.id = id;
     this.title = title;
     this.course = course;
-    this.assigned = assigned;
     this.due = due;
   }
 }
@@ -16,12 +15,55 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: [
-        // placeholder tasks
-        new Task(1, "test1"),
-        new Task(2, "test2"),
-      ],
+      tasks: [],
+      showNewTaskForm: false,
+      form: {
+        title: "",
+        course: "",
+        due: "",
+      },
     }
+
+    this.toggleNewTaskForm = this.toggleNewTaskForm.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleNewTaskForm() {
+    this.setState({ showNewTaskForm: !this.state.showNewTaskForm });
+  }
+
+  deleteTask(i) {
+    let targetTask = this.state.tasks.find(task => task.id === i);
+    let index = this.state.tasks.indexOf(targetTask);
+    let newTasks = [...this.state.tasks];
+    newTasks.splice(index, 1);
+    this.setState({ tasks: newTasks });
+  }
+
+  handleChange(e) {
+    let newForm = this.state.form;
+    newForm[e.target.name] = e.target.value;
+    this.setState({ form: newForm });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let newTasks = [...this.state.tasks];
+    newTasks.push(new Task(nextId++,
+      this.state.form.title,
+      this.state.form.course,
+      this.state.form.due));
+    this.setState({
+      tasks: newTasks,
+      showNewTaskForm: false,
+      form: {
+        title: "",
+        course: "",
+        due: "",
+      }
+    });
   }
 
   render() {
@@ -31,14 +73,76 @@ class App extends React.Component {
           <div id="mt-main-title-container">
             <h1 id="mt-main-title">MicroTask</h1>
           </div>
-          <NewTaskButton />
+          <div id="new-task-button-root">
+            <button
+              className="mt-header-button"
+              onClick={this.toggleNewTaskForm}>
+              {this.state.showNewTaskForm ? "✖" : "New Task"}
+            </button>
+          </div>
           <div id="mt-header-right-placeholder"></div>
         </header>
-        <ul>
+
+        {this.state.showNewTaskForm ?
+          <div id="new-task-form-container" className="mt-card">
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Course:
+                <br />
+                <input
+                type="text"
+                value={this.state.form.course}
+                name="course"
+                onChange={this.handleChange}>
+                </input>
+              </label>
+              <br />
+              <label>
+                Title:
+                <br />
+                <input
+                  type="text"
+                  value={this.state.form.title}
+                  name="title"
+                  onChange={this.handleChange}>
+                </input>
+              </label>
+              <br />
+              <label>
+                Due:
+                <br />
+                <input
+                type="date"
+                value={this.state.form.due}
+                name="due"
+                onChange={this.handleChange}>
+                </input>
+              </label>
+              <br />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+          : null}
+
+        <div id="mt-task-list">
           {this.state.tasks.map((task) =>
-            <li key={task.id}>{task.title}</li>
+            <div key={task.id} className="mt-card">
+              {task.course}
+              <br />
+              {task.title}
+              <br />
+              {task.due}
+              <br />
+              <button className="mt-card-button">edit</button>
+              &nbsp;
+              <button
+                className="mt-card-button"
+                onClick={() => this.deleteTask(task.id)}>
+                delete
+              </button>
+            </div>
           )}
-        </ul>
+        </div>
       </div>
     )
   }
